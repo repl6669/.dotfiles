daf0e93c40186ecaf3e10e982201136c97ea3969
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2025-02-05 21:23:53 +0100

    add fzf config

---
+++ .config/nvim/lua/plugins/fzf.lua
@@ -0,0 +1,31 @@
+return {
+  "ibhagwan/fzf-lua",
+  opts = function(_, opts)
+    local actions = require("fzf-lua").actions
+
+    return vim.tbl_deep_extend("force", opts, {
+      oldfiles = {
+        include_current_session = true,
+      },
+      previewers = {
+        builtin = {
+          -- With this change, the previewer will not add syntax highlighting to files larger than 100KB
+          syntax_limit_b = 1024 * 100, -- 100KB
+        },
+      },
+      files = {
+        cwd_prompt = false,
+        actions = {
+          ["ctrl-i"] = { actions.toggle_ignore },
+          ["ctrl-h"] = { actions.toggle_hidden },
+        },
+      },
+      grep = {
+        actions = {
+          ["ctrl-i"] = { actions.toggle_ignore },
+          ["ctrl-h"] = { actions.toggle_hidden },
+        },
+      },
+    })
+  end,
+}
