cb6fd3dada0232ccd2ae6a679c0ac55bd05216e0
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2025-02-06 00:59:34 +0100

    add dotenv and rg sources to cmp

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -2,11 +2,21 @@ return {
   "hrsh7th/nvim-cmp",
   dependencies = {
     "hrsh7th/cmp-nvim-lsp-signature-help",
+    "SergioRibera/cmp-dotenv",
+    "lukas-reineke/cmp-rg",
   },
   opts = function(_, opts)
     if LazyVim.has("nvim-snippets") then
       table.insert(opts.sources, { name = "nvim_lsp_signature_help" })
     end
+
+    table.insert(opts.sources, { name = "dotenv" })
+
+    table.insert(opts.sources, {
+      name = "rg",
+      -- Try it when you feel cmp performance is poor
+      -- keyword_length = 3,
+    })
 
     opts.window = {
       completion = {
