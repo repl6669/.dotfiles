4c712e8a482721106c8568cde35f1ccc5b901d7c
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2025-02-05 21:24:58 +0100

    update neotree

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -3,10 +3,13 @@ return {
   opts = {
     filesystem = {
       hijack_netrw_behavior = "open_current", -- "open_default" or "open_current" or "disabled",
-      follow_current_file = { enabled = false },
     },
   },
-  config = function()
-    vim.keymap.set("n", "-", ":Neotree current %:p:h:h %:p<CR>") -- Simmulate netrw behaviour
-  end,
+  keys = {
+    {
+      "-",
+      ":Neotree current %:p:h:h %:p<CR>",
+      { noremap = true, silent = true },
+    },
+  },
 }
