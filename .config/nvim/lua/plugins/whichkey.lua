ad3b65f9f7a444715086db1c7cddc7720d738c57
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2025-02-05 21:25:46 +0100

    update whichkey

--- .config/nvim/lua/plugins/whichkey.lua
+++ .config/nvim/lua/plugins/whichkey.lua
@@ -1,7 +1,7 @@
 return {
   "folke/which-key.nvim",
   opts = {
-    defaults = {
+    spec = {
       ["<leader>t"] = { name = "+test" },
       ["<leader>d"] = { name = "+debug" },
       ["<leader>cp"] = { name = "+php" },
