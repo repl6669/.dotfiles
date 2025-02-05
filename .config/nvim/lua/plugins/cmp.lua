c2e319afda76031c3447f678a947bd8a6cf59be4
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2025-02-05 21:23:18 +0100

    update cmp config

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,16 +1,22 @@
-if true then
-  return {}
-end
-
 return {
   "hrsh7th/nvim-cmp",
-  -- dependencies = {
-  --   {
-  --     "tzachar/cmp-fuzzy-buffer",
-  --     dependencies = {
-  --       { "nvim-telescope/telescope-fzf-native.nvim", run = "make" },
-  --       { "tzachar/fuzzy.nvim" },
-  --     },
-  --   },
-  -- },
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp-signature-help",
+  },
+  opts = function(_, opts)
+    if LazyVim.has("nvim-snippets") then
+      table.insert(opts.sources, { name = "nvim_lsp_signature_help" })
+    end
+
+    opts.window = {
+      completion = {
+        border = "rounded",
+        -- winhighlight = "Normal:CmpPmenu,FloatBorder:CmpBorder,CursorLine:PmenuSel,Search:None",
+      },
+      documentation = {
+        border = "rounded",
+        -- winhighlight = "Normal:CmpPmenu,FloatBorder:CmpBorder,CursorLine:PmenuSel,Search:None",
+      },
+    }
+  end,
 }
