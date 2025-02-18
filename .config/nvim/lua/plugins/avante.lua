3bafdab657c17139e2d48674d2b31effdf1e4167
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2025-02-18 09:57:47 +0100

    add avante

---
+++ .config/nvim/lua/plugins/avante.lua
@@ -0,0 +1,57 @@
+return {
+  "yetone/avante.nvim",
+  event = "VeryLazy",
+  lazy = false,
+  version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+  opts = {
+    provider = "openai",
+    openai = {
+      endpoint = "https://api.openai.com/v1",
+      model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
+      timeout = 30000, -- timeout in milliseconds
+      temperature = 0, -- adjust if needed
+      max_tokens = 4096,
+    },
+    file_selector = {
+      provider = "fzf",
+      -- Options override for custom providers
+      provider_opts = {},
+    },
+    hints = { enabled = false },
+    windows = {
+      position = "right",
+      width = 35, -- 35% of the width of the window
+      sidebar_header = {
+        enabled = true, -- true, false to enable/disable the header
+        align = "left", -- left, center, right for title
+        rounded = false,
+      },
+    },
+  },
+  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  build = "make",
+  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+  dependencies = {
+    "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    "zbirenbaum/copilot.lua", -- for providers='copilot'
+    {
+      -- support for image pasting
+      "HakonHarnes/img-clip.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- recommended settings
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          -- required for Windows users
+          use_absolute_path = true,
+        },
+      },
+    },
+  },
+}
