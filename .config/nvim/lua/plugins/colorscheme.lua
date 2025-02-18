42061526c5036277f5db28a10b24d64f6fc49720
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2025-02-18 09:58:04 +0100

    switch back to rose-pine

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -2,7 +2,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "rose-pine",
     },
   },
   {
@@ -139,34 +139,47 @@ return {
       })
     end,
   },
-  -- {
-  --   "rose-pine/neovim",
-  --   name = "rose-pine",
-  --   lazy = false,
-  --   priority = 1000,
-  --   opts = {
-  --     --- @usage 'auto'|'main'|'moon'|'dawn'
-  --     variant = "auto",
-  --     --- @usage 'main'|'moon'|'dawn'
-  --     dark_variant = "main",
-  --
-  --     extend_background_behind_borders = true,
-  --
-  --     enable = {
-  --       legacy_highlights = false,
-  --       migrations = false,
-  --       terminal = true,
-  --     },
-  --
-  --     styles = {
-  --       bold = true,
-  --       italic = true,
-  --       transparency = false,
-  --     },
-  --
-  --     highlight_groups = {
-  --       DashboardHeader = { fg = "love" },
-  --     },
-  --   },
-  -- },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      --- @usage 'auto'|'main'|'moon'|'dawn'
+      variant = "auto",
+      --- @usage 'main'|'moon'|'dawn'
+      dark_variant = "moon",
+
+      extend_background_behind_borders = true,
+
+      enable = {
+        legacy_highlights = false,
+        migrations = false,
+        terminal = true,
+      },
+
+      styles = {
+        bold = true,
+        italic = true,
+        transparency = true,
+      },
+
+      palette = {
+        -- Override the builtin palette per variant
+        moon = {
+          overlay = "#26233a",
+        },
+      },
+
+      highlight_groups = {
+        SnacksDashboardHeader = { fg = "love" },
+        SnacksDashboardIcon = { fg = "gold" },
+        SnacksDashboardDesc = { fg = "rose" },
+        SnacksDashboardTitle = { fg = "rose" },
+        SnacksDashboardSpecial = { fg = "iris" },
+
+        -- DashboardHeader = { fg = "love" },
+      },
+    },
+  },
 }
