3889817d26aa95676b0112457e6de4762b9fb1f9
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2025-02-19 03:14:31 +0100

    update whichkey groups

--- .config/nvim/lua/plugins/whichkey.lua
+++ .config/nvim/lua/plugins/whichkey.lua
@@ -5,6 +5,8 @@ return {
       ["<leader>t"] = { name = "+test" },
       ["<leader>d"] = { name = "+debug" },
       ["<leader>cp"] = { name = "+php" },
+      ["<leader>cpi"] = { name = "+php/import" },
+      ["<leader>cpp"] = { name = "+php/phpstan" },
       ["<leader>cg"] = { name = "+chat-gpt" },
       ["<leader>o"] = { name = "+obsidian" },
       ["<leader>a"] = { name = "+annotations/docblocks" },
