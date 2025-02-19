f5d7ca0e751b186f81f4e4b9ab4d77aee8913c39
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2025-02-19 15:16:20 +0100

    update fidget config

--- .config/nvim/lua/plugins/fidget.lua
+++ .config/nvim/lua/plugins/fidget.lua
@@ -1,6 +1,11 @@
 return {
   "j-hui/fidget.nvim",
   opts = {
+    progress = {
+      display = {
+        done_ttl = 5, -- How long a message should persist after completion
+      },
+    },
     notification = {
       window = {
         winblend = 0, -- Background color opacity in the notification window
