6d4481bd537e7909fd6dcd5e36841c62a93fea34
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2025-02-19 14:59:52 +0100

    add fidget

---
+++ .config/nvim/lua/plugins/fidget.lua
@@ -0,0 +1,11 @@
+return {
+  "j-hui/fidget.nvim",
+  opts = {
+    notification = {
+      window = {
+        winblend = 0, -- Background color opacity in the notification window
+        y_padding = 1,
+      },
+    },
+  },
+}
