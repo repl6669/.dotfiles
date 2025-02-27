3c83435c841c0c296207350d5a267e37c938e2ce
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2025-02-27 18:15:25 +0100

    add markdown util

---
+++ .config/nvim/lua/utils/markdown.lua
@@ -0,0 +1,92 @@
+-- Based on https://github.com/linkarzu/dotfiles-latest/blob/main/neovim/neobean/lua/config/keymaps.lua
+
+local M = {
+  --
+}
+
+M.update_markdown_toc = function(h2)
+  local path = vim.fn.expand("%") -- Expands the current file name to a full path
+  local bufnr = 0 -- The current buffer number, 0 references the current active buffer
+  -- Save the current view
+  -- If I don't do this, my folds are lost when I run this keymap
+  vim.cmd("mkview")
+  -- Retrieves all lines from the current buffer
+  local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+  local toc_exists = false -- Flag to check if TOC marker exists
+  local frontmatter_end = 0 -- To store the end line number of frontmatter
+  -- Check for frontmatter and TOC marker
+  for i, line in ipairs(lines) do
+    if i == 1 and line:match("^---$") then
+      -- Frontmatter start detected, now find the end
+      for j = i + 1, #lines do
+        if lines[j]:match("^---$") then
+          frontmatter_end = j
+          break
+        end
+      end
+    end
+    -- Checks for the TOC marker
+    if line:match("^%s*<!%-%-%s*toc%s*%-%->%s*$") then
+      toc_exists = true
+      break
+    end
+  end
+  -- Inserts H2 heading and <!-- toc --> at the appropriate position
+  if not toc_exists then
+    local insertion_line = 1 -- Default insertion point after first line
+    if frontmatter_end > 0 then
+      -- Find H1 after frontmatter
+      for i = frontmatter_end + 1, #lines do
+        if lines[i]:match("^#%s+") then
+          insertion_line = i + 1
+          break
+        end
+      end
+    else
+      -- Find H1 from the beginning
+      for i, line in ipairs(lines) do
+        if line:match("^#%s+") then
+          insertion_line = i + 1
+          break
+        end
+      end
+    end
+
+    -- Insert the specified headings and <!-- toc --> without blank lines
+    -- Insert the TOC inside a H2 heading right below the main H1 at the top lamw25wmal
+    vim.api.nvim_buf_set_lines(bufnr, insertion_line, insertion_line, false, { h2, "<!-- toc -->" })
+  end
+
+  -- Silently save the file, in case TOC is being created for the first time
+  vim.cmd("silent write")
+  -- Silently run markdown-toc to update the TOC without displaying command output
+  -- vim.fn.system("markdown-toc -i " .. path)
+  -- I want my bulletpoints to be created only as "-" so passing that option as
+  -- an argument according to the docs
+  -- https://github.com/jonschlinkert/markdown-toc?tab=readme-ov-file#optionsbullets
+  vim.fn.system('markdown-toc --bullets "-" -i ' .. path)
+  vim.cmd("edit!") -- Reloads the file to reflect the changes made by markdown-toc
+  vim.cmd("silent write") -- Silently save the file
+  vim.notify("TOC updated and file saved", vim.log.levels.INFO)
+  -- -- In case a cleanup is needed, leaving this old code here as a reference
+  -- -- I used this code before I implemented the frontmatter check
+  -- -- Moves the cursor to the top of the file
+  -- vim.api.nvim_win_set_cursor(bufnr, { 1, 0 })
+  -- -- Deletes leading blank lines from the top of the file
+  -- while true do
+  --   -- Retrieves the first line of the buffer
+  --   local line = vim.api.nvim_buf_get_lines(bufnr, 0, 1, false)[1]
+  --   -- Checks if the line is empty
+  --   if line == "" then
+  --     -- Deletes the line if it's empty
+  --     vim.api.nvim_buf_set_lines(bufnr, 0, 1, false, {})
+  --   else
+  --     -- Breaks the loop if the line is not empty, indicating content or TOC marker
+  --     break
+  --   end
+  -- end
+  -- Restore the saved view (including folds)
+  vim.cmd("loadview")
+end
+
+return M
