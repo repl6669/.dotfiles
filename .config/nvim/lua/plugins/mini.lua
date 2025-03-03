85331a8583b711ba54dd3cf23f249aabadb8a441
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2025-03-03 21:57:37 +0100

    replace plugins with mini and merge to mini.lua

---
+++ .config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,53 @@
+return {
+  {
+    "echasnovski/mini.surround",
+    optional = true,
+    opts = {
+      mappings = {
+        add = "gza", -- Add surrounding in Normal and Visual modes
+        delete = "gzd", -- Delete surrounding
+        find = "gzf", -- Find surrounding (to the right)
+        find_left = "gzF", -- Find surrounding (to the left)
+        highlight = "gzh", -- Highlight surrounding
+        replace = "gzr", -- Replace surrounding
+        update_n_lines = "gzn", -- Update `n_lines`
+      },
+    },
+    keys = {
+      { "gz", "", desc = "+surround" },
+    },
+  },
+
+  {
+    "echasnovski/mini.hipatterns",
+    event = "BufReadPre",
+    opts = {
+      highlighters = {
+        hsl_color = {
+          pattern = "hsl%(%d+,? %d+%%?,? %d+%%?%)",
+          group = function(_, match)
+            local utils = require("utils.hsl")
+            --- @type string, string, string
+            local nh, ns, nl = match:match("hsl%((%d+),? (%d+)%%?,? (%d+)%%?%)")
+            --- @type number?, number?, number?
+            local h, s, l = tonumber(nh), tonumber(ns), tonumber(nl)
+            --- @type string
+            local hex_color = utils.hslToHex(h, s, l)
+            return MiniHipatterns.compute_hex_color_group(hex_color, "bg")
+          end,
+        },
+      },
+    },
+  },
+
+  {
+    "echasnovski/mini.splitjoin",
+    opts = {
+      mappings = {
+        toggle = "gS",
+        split = "",
+        join = "",
+      },
+    },
+  },
+}
