c7f3be4f132602913eb64f8e3985f19febed88ab
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2025-03-07 05:13:32 +0100

    add yazi y command

--- commands.zsh
+++ commands.zsh
@@ -12,43 +12,22 @@ lg() {
     fi
 }
 
-phpv() {
-    valet stop
-    brew unlink php@8.1 php@8.2 # php@7.4
-    brew link --force --overwrite $1
-    brew services start $1
-    composer global update
-    rm -f ~/.config/valet/valet.sock
-    valet install
+# Yazi
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	yazi "$@" --cwd-file="$tmp"
+	if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		builtin cd -- "$cwd"
+	fi
+	rm -f -- "$tmp"
 }
 
 myip() {
     ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}'
 }
 
-# alias php74="phpv php@7.4"
-# alias php80="phpv php@8.0"
-alias php81="phpv php@8.1"
-alias php82="phpv php@8.2"
-
 composer-link() {
     composer config repositories.local '{"type": "path", "url": "'$1'"}' --file composer.json
-}
-
-function stage {
-    SOURCE_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-    echo "pull existing changes..." &&
-    git pull &&
-    echo "checking out staging..." &&
-    git checkout staging &&
-    echo "pulling staging..." &&
-    git pull
-    echo "merging $SOURCE_BRANCH.." &&
-    git merge $SOURCE_BRANCH --no-edit &&
-    echo "pushing to staging..." &&
-    git push &&
-    echo "checkout back to $SOURCE_BRANCH" &&
-    git checkout $SOURCE_BRANCH
 }
 
 # Zellij
