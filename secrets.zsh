4fee81d6c3ce7e8ef3580c2554a18af776762c06
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2025-05-14 01:28:06 +0200

    add deep seek api key to secrets

--- secrets.zsh
+++ secrets.zsh
@@ -2,4 +2,4 @@ export OPENAI_API_KEY="op://Private/op54pbs2bcvrdekea3uqg4p62a/API key"
 export ANTHROPIC_API_KEY="op://Private/Anthropic/API key"
 export TAVILY_API_KEY="op://Private/Tavily/API key"
 export GROQ_API_KEY="op://Private/Groq/API key"
-
+export DEEPSEEK_API_KEY="op://Secret/DeepSeek/API key"
