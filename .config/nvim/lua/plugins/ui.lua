c22bcf2fe8af69dbad36b16f8451604991bf8ec0
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2025-06-17 01:26:12 +0200

    use random snacks dashboard header color on each nvim start

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -13,12 +13,27 @@ return {
     lazy = false,
     priority = 1000,
     opts = {
+      style = "void",
       transparent = true,
-      highlight_groups = {
-
-        -- Dashboard
-        SnacksDashboardHeader = { fg = "green100" },
+      styles = {
+        floats = "transparent",
+        sidebars = "transparent",
       },
+      on_highlights = function(highlights, colors)
+        -- Dashboard
+        local header_colors = {
+          colors.green100,
+          colors.orange100,
+          colors.purple100,
+          colors.blue100,
+          colors.red100,
+          colors.yellow100,
+        }
+        -- Seed the random number generator with the current time for randomness on each nvim start
+        math.randomseed(os.time())
+        local random_color = header_colors[math.random(#header_colors)]
+        highlights.SnacksDashboardHeader = { fg = random_color }
+      end,
     },
   },
 }
