35f60917561160dd69c0b05e4bdf5eaab54e8fd1
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2025-06-17 23:22:17 +0200

    update autocmds

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -27,16 +27,16 @@ vim.api.nvim_create_autocmd({ "BufEnter" }, {
   end,
 })
 
-local cacher = require("vectorcode.config").get_cacher_backend()
-
-vim.api.nvim_create_autocmd({ "LspAttach" }, {
-  callback = function()
-    local bufnr = vim.api.nvim_get_current_buf()
-    cacher.async_check("config", function()
-      cacher.register_buffer(bufnr, {
-        n_query = 12,
-      })
-    end, nil)
-  end,
-  desc = "Register buffer for VectorCode",
-})
+-- local cacher = require("vectorcode.config").get_cacher_backend()
+--
+-- vim.api.nvim_create_autocmd({ "LspAttach" }, {
+--   callback = function()
+--     local bufnr = vim.api.nvim_get_current_buf()
+--     cacher.async_check("config", function()
+--       cacher.register_buffer(bufnr, {
+--         n_query = 10,
+--       })
+--     end, nil)
+--   end,
+--   desc = "Register buffer for VectorCode",
+-- })
