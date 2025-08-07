ad4a5088526aaa5456b1d18a82926038f3fb7233
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2025-08-07 23:25:23 +0200

    treesitter can now install blade by itself

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -41,6 +41,7 @@ return {
   opts = {
     ensure_installed = {
       "bash",
+      "blade",
       "c",
       "diff",
       "html",
@@ -95,34 +96,4 @@ return {
       },
     },
   },
-  config = function(_, opts)
-    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-
-    parser_config.blade = {
-      install_info = {
-        url = "https://github.com/EmranMR/tree-sitter-blade",
-        files = { "src/parser.c" },
-        branch = "main",
-      },
-      filetype = "blade",
-    }
-
-    vim.filetype.add({
-      pattern = {
-        [".*%.blade%.php"] = "blade",
-      },
-    })
-
-    local bladeGrp
-    vim.api.nvim_create_augroup("BladeFiltypeRelated", { clear = true })
-    vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-      pattern = "*.blade.php",
-      group = bladeGrp,
-      callback = function()
-        vim.opt.filetype = "blade"
-      end,
-    })
-
-    require("nvim-treesitter.configs").setup(opts)
-  end,
 }
