408d03ef7b3a0865e909c65ae24fde0c61379b33
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2025-09-24 20:19:26 +0200

    fix neotree

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -8,7 +8,27 @@ return {
   keys = {
     {
       "-",
-      ":Neotree position=current<CR>",
+      function()
+        local bufname = vim.api.nvim_buf_get_name(0)
+        local dir
+        if bufname ~= "" then
+          local grandparent = vim.fn.fnamemodify(bufname, ":p:h:h")
+          if vim.fn.isdirectory(grandparent) == 1 then
+            dir = grandparent
+          else
+            dir = vim.fn.fnamemodify(bufname, ":p:h")
+          end
+        else
+          dir = (vim.uv or vim.loop).cwd()
+        end
+
+        local file = bufname ~= "" and vim.fn.fnamemodify(bufname, ":p") or nil
+        require("neo-tree.command").execute({
+          position = "current",
+          dir = dir,
+          reveal_file = file,
+        })
+      end,
       { noremap = true, silent = true },
     },
   },
